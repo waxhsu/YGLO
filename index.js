b1dc54e874a0787950a103d57e817c49e262e6e0
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-10-17 22:51:42 -0400

    added CPS

--- index.js
+++ index.js
@@ -1,85 +1,99 @@
-let points = 0;
+// Initialize variables
+let jobApplications = 0;
+let autoApplications = [];
 let clickValue = 1;
-let upgradeCost = 10;
-let upgrades = 0;
-let autoClickers = [
-  { name: "AutoClicker 1", cost: 50, rate: 1 },
-  { name: "AutoClicker 2", cost: 100, rate: 2 },
-  { name: "AutoClicker 3", cost: 200, rate: 4 },
-  { name: "AutoClicker 4", cost: 500, rate: 8 },
-  { name: "AutoClicker 5", cost: 1000, rate: 16 },
+let totalClicksPerSecond = 0;
+
+// Get HTML elements
+const jobApplicationsElement = document.getElementById("job-applications");
+const clickButton = document.getElementById("click-button");
+const shop = document.getElementById("shop");
+const autoApplicationsElement = document.getElementById("autoApplications");
+
+// Function to update the job application count on the screen
+function updateJobApplications() {
+    jobApplicationsElement.textContent = `${Math.round(jobApplications)} Job Applications`;
+}
+
+// Function to update the CPS (Clicks Per Second) display
+function updateCPSDisplay() {
+  const cpsDisplay = document.getElementById("cps-display");
+  cpsDisplay.textContent = `CPS: ${totalClicksPerSecond.toFixed(2)}`; // Display CPS rounded to 2 decimal places
+}
+
+
+// Function to update the AutoApplications in the shop
+function updateShop() {
+    autoApplicationsElement.innerHTML = "";
+    for (const app of autoApplications) {
+        const buyButton = document.createElement("button");
+        buyButton.textContent = `${app.name} - Cost: ${app.cost} Job Applications - CPS: ${app.clicksPerSecond}`;
+        buyButton.addEventListener("click", () => buyAutoApplication(app));
+        autoApplicationsElement.appendChild(buyButton);
+    }
+}
+
+// Function to buy an AutoApplication
+function buyAutoApplication(app) {
+  if (jobApplications >= app.cost) {
+      jobApplications -= app.cost;
+      app.count += 1;
+      app.cost = Math.floor(app.cost * 1.1); // Cost scales exponentially
+      updateJobApplications();
+      updateShop();
+      calculateTotalClicksPerSecond();
+      updateCPSDisplay(); // Update CPS when purchasing
+  }
+}
+
+// Function to calculate the total clicks per second
+function calculateTotalClicksPerSecond() {
+    totalClicksPerSecond = 0;
+    for (const app of autoApplications) {
+        totalClicksPerSecond += app.count * app.clicksPerSecond;
+    }
+}
+
+// Function to add job applications automatically based on CPS
+function autoGenerateJobApplications() {
+  jobApplications += totalClicksPerSecond;
+  updateJobApplications();
+  updateCPSDisplay(); // Update CPS when generating
+}
+
+// Add a click event listener to the button
+clickButton.addEventListener("click", () => {
+    jobApplications += clickValue;
+    updateJobApplications();
+});
+
+// Initialize the shop with AutoApplications
+autoApplications = [
+    {
+        id: 1,
+        name: "Try Harder",
+        cost: 10,
+        clicksPerSecond: 0.1,
+        count: 0,
+    },
+    {
+        id: 2,
+        name: "LinkedIn Premium",
+        cost: 50,
+        clicksPerSecond: 1,
+        count: 0,
+    },
+    {
+        id: 3,
+        name: "Outsource to Fiverr",
+        cost: 1000,
+        clicksPerSecond: 5,
+        count: 0,
+    },
 ];
 
-let autoClickerRunning = false;
-
-const pointsElement = document.getElementById("points");
-const clickButton = document.getElementById("clickButton");
-const buyUpgradeButton = document.getElementById("buyUpgrade");
-const upgradesElement = document.getElementById("upgrades");
-const shopElement = document.getElementById("shop");
-
-clickButton.addEventListener("click", () => {
-  points += clickValue;
-  updatePointsDisplay();
-});
-
-buyUpgradeButton.addEventListener("click", () => {
-  if (points >= upgradeCost) {
-    points -= upgradeCost;
-    clickValue += 1;
-    upgrades += 1;
-    upgradeCost = Math.floor(upgradeCost * 1.5);
-    updatePointsDisplay();
-    updateUpgradesDisplay();
-
-    if (!autoClickerRunning) {
-      startAutoClickers();
-      autoClickerRunning = true;
-    }
-  }
-});
-
-function updatePointsDisplay() {
-  pointsElement.innerText = points;
-}
-
-function updateUpgradesDisplay() {
-  upgradesElement.innerText = upgrades;
-  buyUpgradeButton.innerText = `Buy Upgrade (Cost: ${upgradeCost} points)`;
-}
-
-function updateShopDisplay() {
-  shopElement.innerHTML = "";
-  autoClickers.forEach((autoClicker, index) => {
-    const button = document.createElement("button");
-    button.innerText = `${autoClicker.name} (Cost: ${autoClicker.cost} points, Rate: ${autoClicker.rate}/s)`;
-    button.addEventListener("click", () => {
-      if (points >= autoClicker.cost) {
-        points -= autoClicker.cost;
-        clickValue += autoClicker.rate;
-        autoClickers[index].cost = Math.floor(autoClicker.cost * 1.5);
-        updatePointsDisplay();
-        updateShopDisplay();
-
-        if (!autoClickerRunning) {
-          startAutoClickers();
-          autoClickerRunning = true;
-        }
-      }
-    });
-    shopElement.appendChild(button);
-  });
-}
-
-function startAutoClickers() {
-  setInterval(() => {
-    autoClickers.forEach((autoClicker) => {
-      points += autoClicker.rate;
-    });
-    updatePointsDisplay();
-  }, 1000);
-}
-
-updatePointsDisplay();
-updateUpgradesDisplay();
-updateShopDisplay();
+// Initialize the game
+updateShop();
+updateJobApplications();
+updateCPSDisplay(); // Display initial CPS
+setInterval(autoGenerateJobApplications, 1000);
